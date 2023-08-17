d8a04b3e33e1f39629d1993c65b5620102b1d241
natured_amends0p <natured_amends0p@icloud.com>
2023-08-17 18:04:43 +0000

    save

--- test/common-utils/test.sh
+++ test/common-utils/test.sh
@@ -12,7 +12,7 @@ check "git" git  --version
 check "zsh" zsh --version
 check "ps" ps --version
 check "Oh My Zsh! theme" test -e $HOME/.oh-my-zsh/custom/themes/devcontainers.zsh-theme
-check "zsh theme symlink" test -e $HOME/.oh-my-zsh/custom/themes/codespaces.zsh-theme
+check "zsh theme filename" test -e $HOME/.oh-my-zsh/custom/themes/codespaces.zsh-theme
 check "oh-my-zsh executes" zsh -c 'echo $0 | grep zsh' 
 
 # Report result
